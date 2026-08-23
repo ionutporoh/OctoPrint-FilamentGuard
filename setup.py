f87679b40b758e53dac16b58d8881b5ceae950b3
Ionut Poroh <ionut.poroh@gmail.com>
2026-08-23 14:52:30 +0300

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="OctoPrint-FilamentGuard",
-    version="0.1.1",
+    version="0.1.2",
     description=(
         "Filament jam/runout detection using a hall-effect pulse sensor, "
         "with no-motion and under-extrusion (partial clog) detection modes."
